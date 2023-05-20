ec32e3f4fdf6f67e48c49ecac816567bfda1a5b4
Ivan <Koryakin.Ivan90@yandex.ru>
2023-05-21 00:20:57 +0900

    Работа сделана

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int max = numA;
+if (max > numB)
+    System.Console.WriteLine("Max = "+max);
+else
+    // max = numB;
+    System.Console.WriteLine("Max = "+numB);
---
+++ HW_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+int max = numA;
+int sum = 0;
+
+while (sum < 3)
+    {
+        if (max<numB)
+            {
+                max = numB;
+                sum++;
+            }
+        else if (max<numC)
+            {
+                max = numC;
+                sum++;
+            }
+        else
+            sum++;
+    }
+
+System.Console.WriteLine("Наибольшее число: "+max);
